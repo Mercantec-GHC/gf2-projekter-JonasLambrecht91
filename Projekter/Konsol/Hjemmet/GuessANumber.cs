643ab994f6d56c93af0f359700a1f700c7fa93cf
Jonas <jona7268@edu.mercantec.dk>
2025-09-10 15:04:43 +0200

    Update GuessANumber.cs

--- Projekter/Konsol/Hjemmet/GuessANumber.cs
+++ Projekter/Konsol/Hjemmet/GuessANumber.cs
@@ -5,7 +5,13 @@ namespace Hjemmet
         public void Start()
         {
             Console.WriteLine("Gæt et tal er ikke implementeret endnu.");
+
+            Random simonHjerne = new Random();
+            int simonTal = simonHjerne.Next(1, 1000000);
+            Console.WriteLine($"Pssst, Simon tænker måske på tallet: {simonTal}");
+
             Console.ReadKey();
+
         }
     }
 }
