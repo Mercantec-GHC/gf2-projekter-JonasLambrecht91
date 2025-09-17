5fbcc3c697bf02c65ef7ad3914401f1dd65659cd
Jonas <jona7268@edu.mercantec.dk>
2025-09-17 14:50:54 +0200

    Update RockPaperScissors.cs

--- Projekter/Konsol/Hjemmet/RockPaperScissors.cs
+++ Projekter/Konsol/Hjemmet/RockPaperScissors.cs
@@ -4,8 +4,59 @@ namespace Hjemmet
     {
         public void Start()
         {
-            Console.WriteLine("Sten, Saks, Papir er ikke implementeret endnu.");
-            Console.ReadKey();
+         Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+            String answer;
+
+            while (playAgain)
+            {
+                Console.Clear();
+                Console.WriteLine("Rock, Paper, Scissors Game!");
+                Console.WriteLine("Choose [R]ock, [P]aper, or [S]cissors:");
+                player = Console.ReadLine().ToUpper();
+                int computerChoice = random.Next(1, 4);
+                switch (computerChoice)
+                {
+                    case 1:
+                        computer = "R";
+                        break;
+                    case 2:
+                        computer = "P";
+                        break;
+                    case 3:
+                        computer = "S";
+                        break;
+                    default:
+                        computer = "R"; // Default case, should not occur
+                        break;
+                }
+                Console.WriteLine($"Computer chose: {computer}");
+                if (player == computer)
+                {
+                    Console.WriteLine("It's a tie!");
+                }
+                else if ((player == "R" && computer == "S") ||
+                         (player == "P" && computer == "R") ||
+                         (player == "S" && computer == "P"))
+                {
+                    Console.WriteLine("You win!");
+                }
+                else if ((player == "R" && computer == "P") ||
+                         (player == "P" && computer == "S") ||
+                         (player == "S" && computer == "R"))
+                {
+                    Console.WriteLine("Computer wins!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! Please choose R, P, or S.");
+                }
+                Console.WriteLine("Do you want to play again? (Y/N)");
+                answer = Console.ReadLine().ToUpper();
+                playAgain = (answer == "Y");
+            }
         }
     }
 }
